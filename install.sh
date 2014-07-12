cbd53ef04c8756e211304ed6a16a009f63cbebba
Ryan Sullivan <ryan@ryansully.com>
2014-07-11 19:44:37 -0700

    add ST3 settings

--- install.sh
+++ install.sh
@@ -37,5 +37,6 @@ setup_symlink ".npmrc" "$HOME"
 setup_symlink ".vim" "$HOME"
 setup_symlink ".vimrc" "$HOME"
 setup_symlink "sublime-text-2/User" "$HOME/.config/sublime-text-2/Packages"
+setup_symlink "sublime-text-3/User" "$HOME/.config/sublime-text-3/Packages"
 
 echo "Installation complete!"
