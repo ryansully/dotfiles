a23657e282a76783975c12066200700009d0215d
Ryan Sullivan <ryan@ryansully.com>
2019-01-02 03:35:42 -0800

    Fix build dependency for rbenv Ruby compilation

--- install/ruby.sh
+++ install/ruby.sh
@@ -9,6 +9,10 @@ fi
 # Install latest (stable) Ruby
 ruby_latest=$(rbenv install -l 2>/dev/null | awk '$1 ~ /^[0-9.]*$/ {latest=$1} END {print latest}')
 if [ $ruby_latest != `rbenv version | awk '{print $1;}'` ]; then
+  if [ `uname` = 'Linux' ]; then
+    sudo apt-get install -y libssl-dev
+  fi
+
   rbenv install $ruby_latest
   rbenv global $ruby_latest
   rbenv rehash
