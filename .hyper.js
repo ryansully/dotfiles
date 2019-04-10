86f2da5825a68b3c5f98539393d41fdb5564c564
Ryan Sullivan <ryan@ryansully.com>
2019-04-10 04:39:19 -0700

    Add Fira Code to Hyper fonts

--- .hyper.js
+++ .hyper.js
@@ -12,7 +12,7 @@ module.exports = {
     fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: '"FuraCode Nerd Font", "FuraCode NF", "Source Code Pro", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: '"FuraCode Nerd Font", "FuraCode NF", "Fira Code", "Source Code Pro", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
