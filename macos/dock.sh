4530312bc474c107fdc496d743ef6e1887d7af00
Ryan Sullivan <ryan@ryansully.com>
2019-05-22 22:11:31 -0700

    Add Firefox and VS Code Insiders to macOS Dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -2,8 +2,9 @@
 
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
-dockutil --no-restart --add "/Applications/Mail.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code - Insiders.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
 
 killall Dock
